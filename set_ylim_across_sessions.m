b312086cbd96dffb407d11929fe7fcbfff37073e
Xing Chen <x.chen1@ncl.ac.uk>
2013-03-12 15:25:28 +0000

    shared ROC/CRF routine

--- set_ylim_across_sessions.m
+++ set_ylim_across_sessions.m
@@ -1,4 +1,4 @@
-function set_ylim_across_sessions(chNum,numsessions,sampleContrast,testContrast,c50,chSSE,yLimCRF,fileText)
+function set_ylim_across_sessions(chNum,numsessions,sampleContrast,testContrast,c50,chSSE,yLimData,fileText)
 if ischar(chNum)
     titleText=chNum;
 else
@@ -7,16 +7,16 @@ end
 %standardises y-limits across all subplots
 for i=1:numsessions
     subplot(ceil(numsessions/5),5,i);
-    ylim([min(yLimCRF(:,1)) max(yLimCRF(:,2))]);
-    line(c50(1,i),0:max(yLimCRF(:,2))/100:max(yLimCRF(:,2)),'Color','r');
-    line(sampleContrast,0:max(yLimCRF(:,2))/100:max(yLimCRF(:,2)));
+    ylim([min(yLimData(:,1)) max(yLimData(:,2))]);
+    line(c50(1,i),0:max(yLimData(:,2))/100:max(yLimData(:,2)),'Color','r');
+    line(sampleContrast,0:max(yLimData(:,2))/100:max(yLimData(:,2)));
     if i==1
-        crfname=[titleText,' ',num2str(sampleContrast),' ',fileText];
-        ptext=sprintf('%s',crfname);
+        dataName=[titleText,' ',num2str(sampleContrast),' ',fileText];
+        ptext=sprintf('%s',dataName);
         orient landscape
         yLimVals=get(gca,'YLim');
         text('Position',[-10 yLimVals(2)+0.2*(yLimVals(2)-yLimVals(1))],'FontSize',9,'String',ptext);
     end
     ptext=sprintf('SSE: %.3f',chSSE(i,2));
-    text('Position',[testContrast(end)/2 min(yLimCRF(:,1))+(max(yLimCRF(:,2))-min(yLimCRF(:,1)))/4],'FontSize',9,'String',ptext);
+    text('Position',[testContrast(end)/2 min(yLimData(:,1))+(max(yLimData(:,2))-min(yLimData(:,1)))/4],'FontSize',9,'String',ptext);
 end
