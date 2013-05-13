3262b7fb1555c5f11dc20b46949ed9ba6ba082eb
Xing Chen <x.chen1@ncl.ac.uk>
2013-05-13 15:33:42 +0100

    uniform trial inclusion 311 333 398 451

--- remove_empty_398.m
+++ remove_empty_398.m
@@ -8,12 +8,18 @@ area='v1_2';
 sessions=[398 451];
 sessions=[ 451];
 minTrials=[50 60];
+if strcmp(area,'v1_2')
+    numconds=12;
+else
+    numconds=14;
+end
+includeRows=[7 5 7 6 6 8 15 7 10 19 6 6];
 channels=main_channels(animal,area);
 for sessInd=1:length(sessions)
     for i=1:length(channels)
         loadText=['load F:\PL\spikeData\blanco\',num2str(channels(i)),'_',num2str(sessions(sessInd)),'_30.mat'];
         eval(loadText);
-        for condInd=1:14
+        for condInd=1:numconds
             emptyRows=[];
             for rowInd=1:size(matarray{condInd,2},1)
                 if isempty(matarray{condInd,2}{rowInd})||length(matarray{condInd,4}{rowInd})<10
@@ -22,8 +28,10 @@ for sessInd=1:length(sessions)
             end
             firstEmpty=emptyRows(diff(emptyRows)==1);%first empty cell, out of sequence of at least 2 consequtive indices
             firstEmpty=firstEmpty(firstEmpty>minTrials(sessInd));
-            for columnInd=1:size(matarray,2)
-                matarray{condInd,columnInd}=matarray{condInd,columnInd}(1:firstEmpty(1)-1);
+            if ~isempty(firstEmpty)
+                for columnInd=1:size(matarray,2)
+                    matarray{condInd,columnInd}=matarray{condInd,columnInd}(1:firstEmpty(1)-1);
+                end
             end
         end
         saveText=['save F:\PL\spikeData\blanco\',num2str(channels(i)),'_',num2str(sessions(sessInd)),'_30.mat matarray'];
