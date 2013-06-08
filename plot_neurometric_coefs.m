55532d7484cf17614b2139b7d5ffe6e295ca86ea
Xing Chen <x.chen1@ncl.ac.uk>
2013-06-08 18:50:28 +0100

    partial correlation coefs

--- plot_neurometric_coefs.m
+++ plot_neurometric_coefs.m
@@ -1,4 +1,4 @@
-function plot_neurometric_coefs(animal,area,chNum,appendText,startEndTime,slopeNeuro,c50,plotDiffC50_30,diffc50,minRate,maxRate,sessionSorted1,analysisTypeText,example_ch_54,excludeSessHighSSE,excludeOutliers,writeCoefs,slSigmaMultiple,c50SigmaMultiple)
+function plot_neurometric_coefs(animal,area,chNum,appendText,startEndTime,slopeNeuro,c50,plotDiffC50_30,diffc50,minRate,maxRate,sessionSorted1,analysisTypeText,example_ch_54,excludeSessHighSSE,excludeOutliers,writeCoefs,slSigmaMultiple,c50SigmaMultiple,calcPartial)
 % Handles plotting of correlation coefficients
 if plotDiffC50_30==1
     fig=figure('Color',[1,1,1],'Units','Normalized','Position',[0.12, 0.08, 0.8, 0.8]);
@@ -135,122 +135,172 @@ a=[sessionSorted1' slopeNeuro'];
 c50=real(c50);
 b=[sessionSorted1' c50'];
 c=[slopeNeuro' matchPsycho'];
-[coefficients1 p1]=corrcoef(a);
-[coefficients2 p2]=corrcoef(b);
-[coefficients3 p3]=corrcoef(c);
-if plotDiffC50_30==1
-    diffc50=real(diffc50);
-    d=[sessionSorted1' diffc50'];
-    [coefficients4 p4]=corrcoef(d);
-end
-e=[sessionSorted1' minRate'];
-f=[sessionSorted1' maxRate'];
-[coefficients5 p5]=corrcoef(e);
-[coefficients6 p6]=corrcoef(f);
-if length(coefficients1)>1
+if calcPartial==0
+    [coefficients1 p1]=corrcoef(a);
+    [coefficients2 p2]=corrcoef(b);
+    [coefficients3 p3]=corrcoef(c);
     if plotDiffC50_30==1
-        coefficients(1,1:6)=[coefficients1(2) coefficients2(2) coefficients3(2) coefficients4(2) coefficients5(2) coefficients6(2)];
-        coefficients(2,1:6)=[p1(2) p2(2) p3(2) p4(2) p5(2) p6(2)]
-        subplot(2,3,1);
-        ptext=sprintf('r= %f  p= %f',coefficients(1,1),coefficients(2,1));
-        yLimVals=get(gca,'YLim');
-        xLimVals=get(gca,'XLim');
-        text('Position',[xLimVals(1) yLimVals(1)-0.1*(yLimVals(2)-yLimVals(1))],'FontSize',9,'String',ptext);
-        subplot(2,3,4);
-        ptext=sprintf('r= %f  p= %f',coefficients(1,2),coefficients(2,2));
-        yLimVals=get(gca,'YLim');
-        xLimVals=get(gca,'XLim');
-        text('Position',[xLimVals(1) yLimVals(1)-0.1*(yLimVals(2)-yLimVals(1))],'FontSize',9,'String',ptext);
-        subplot(2,3,2);
-        ptext=sprintf('r= %f  p= %f',coefficients(1,3),coefficients(2,3));
-        yLimVals=get(gca,'YLim');
-        xLimVals=get(gca,'XLim');
-        text('Position',[xLimVals(1) yLimVals(1)-0.1*(yLimVals(2)-yLimVals(1))],'FontSize',9,'String',ptext);
-        subplot(2,3,5);
-        ptext=sprintf('r= %f  p= %f',coefficients(1,4),coefficients(2,4));
-        yLimVals=get(gca,'YLim');
-        xLimVals=get(gca,'XLim');
-        text('Position',[xLimVals(1) yLimVals(1)-0.1*(yLimVals(2)-yLimVals(1))],'FontSize',9,'String',ptext);
-        subplot(2,3,6);
-        ptext=sprintf('r= %f  p= %f',coefficients(1,5),coefficients(2,5));
-        yLimVals=get(gca,'YLim');
-        xLimVals=get(gca,'XLim');
-        text('Position',[xLimVals(1) yLimVals(1)-0.1*(yLimVals(2)-yLimVals(1))],'FontSize',9,'String',ptext);
-        subplot(2,3,3);
-        ptext=sprintf('r= %f  p= %f',coefficients(1,6),coefficients(2,6));
-        yLimVals=get(gca,'YLim');
-        xLimVals=get(gca,'XLim');
-        text('Position',[xLimVals(1) yLimVals(1)-0.1*(yLimVals(2)-yLimVals(1))],'FontSize',9,'String',ptext);
-    else
-        coefficients(1,1:5)=[coefficients1(2) coefficients2(2) coefficients3(2) coefficients5(2) coefficients6(2)];
-        coefficients(2,1:5)=[p1(2) p2(2) p3(2) p5(2) p6(2)]
-        subplot(1,3,1);
-        ptext=sprintf('r= %f  p= %f',coefficients(1,1),coefficients(2,1));
-        yLimVals=get(gca,'YLim');
-        xLimVals=get(gca,'XLim');
-        text('Position',[xLimVals(1) yLimVals(1)-0.1*(yLimVals(2)-yLimVals(1))],'FontSize',9,'String',ptext);
-        subplot(1,3,2);
-        ptext=sprintf('r= %f  p= %f',coefficients(1,2),coefficients(2,2));
-        yLimVals=get(gca,'YLim');
-        xLimVals=get(gca,'XLim');
-        text('Position',[xLimVals(1) yLimVals(1)-0.1*(yLimVals(2)-yLimVals(1))],'FontSize',9,'String',ptext);
-        subplot(1,3,3);
-        ptext=sprintf('r= %f  p= %f',coefficients(1,3),coefficients(2,3));
-        yLimVals=get(gca,'YLim');
-        xLimVals=get(gca,'XLim');
-        text('Position',[xLimVals(1) yLimVals(1)-0.1*(yLimVals(2)-yLimVals(1))],'FontSize',9,'String',ptext);
-        subplot(1,3,4);
-        ptext=sprintf('r= %f  p= %f',coefficients(1,4),coefficients(2,4));
-        yLimVals=get(gca,'YLim');
-        xLimVals=get(gca,'XLim');
-        text('Position',[xLimVals(1) yLimVals(1)-0.1*(yLimVals(2)-yLimVals(1))],'FontSize',9,'String',ptext);
+        diffc50=real(diffc50);
+        d=[sessionSorted1' diffc50'];
+        [coefficients4 p4]=corrcoef(d);
     end
-    set(gcf,'PaperPositionMode','auto')
-    if isempty(chNum)
-        chText='mean_across_channels';
-    else
-        chText=num2str(chNum);
+    e=[sessionSorted1' minRate'];
+    f=[sessionSorted1' maxRate'];
+    [coefficients5 p5]=corrcoef(e);
+    [coefficients6 p6]=corrcoef(f);
+end
+
+if calcPartial==2
+    %partial correlations:
+    [coefficients1(2) p1(2)]=partialcorr(slopeNeuro',sessionSorted1',[c50' minRate' maxRate']);
+    [coefficients2(2) p2(2)]=partialcorr(c50',sessionSorted1',[slopeNeuro' minRate' maxRate']);
+    [coefficients3(2) p3(2)]=partialcorr(slopeNeuro',matchPsycho',[c50' minRate' maxRate']);
+    [coefficients4(2) p4(2)]=partialcorr(diffc50',sessionSorted1',[slopeNeuro' minRate' maxRate']);
+    [coefficients5(2) p5(2)]=partialcorr(minRate',sessionSorted1',[slopeNeuro' c50' maxRate']);
+    [coefficients6(2) p6(2)]=partialcorr(maxRate',sessionSorted1',[slopeNeuro' c50' minRate']);
+end
+
+goodSlope=~isnan(slopeNeuro);
+goodPNE=~isnan(c50);
+goodSession=goodSlope+goodPNE==2;
+slopeNeuroFinal=slopeNeuro(goodSession);
+c50Final=c50(goodSession);
+sessionSorted1Final=sessionSorted1(goodSession);
+if length(slopeNeuroFinal)>=0.8*length(sessionSorted1)
+    if calcPartial==1
+        %partial correlations for just slope and PNE:
+        [coefficients1(2) p1(2)]=partialcorr(slopeNeuroFinal',sessionSorted1Final',c50Final');
+        [coefficients2(2) p2(2)]=partialcorr(c50Final',sessionSorted1Final',slopeNeuroFinal');
     end
-    subFolderName=[analysisTypeText,'_coef_images'];
-    if excludeSessHighSSE==0
-            coefImagename=[chText,appendText,startEndTime,'_',analysisTypeText,'_coefs_',area];
-    elseif excludeSessHighSSE==1
-        if excludeOutliers==0
-            coefImagename=[chText,appendText,startEndTime,'_',analysisTypeText,'_coefs_',area,'goodSSE'];
-        elseif excludeOutliers==1
-            coefImagename=[chText,appendText,startEndTime,'_',analysisTypeText,'_coefs_',area,'goodSSE_no_outliers_sl',num2str(slSigmaMultiple),'_C50',num2str(c50SigmaMultiple)];
+    
+    if length(coefficients1)>=1
+        if plotDiffC50_30==1
+            if calcPartial==1
+                coefficients(1,1:2)=[coefficients1(2) coefficients2(2)];
+                coefficients(2,1:2)=[p1(2) p2(2)]
+                subplot(2,3,1);
+                ptext=sprintf('r= %f  p= %f',coefficients(1,1),coefficients(2,1));
+                yLimVals=get(gca,'YLim');
+                xLimVals=get(gca,'XLim');
+                text('Position',[xLimVals(1) yLimVals(1)-0.1*(yLimVals(2)-yLimVals(1))],'FontSize',9,'String',ptext);
+                subplot(2,3,4);
+                ptext=sprintf('r= %f  p= %f',coefficients(1,2),coefficients(2,2));
+                yLimVals=get(gca,'YLim');
+                xLimVals=get(gca,'XLim');
+                text('Position',[xLimVals(1) yLimVals(1)-0.1*(yLimVals(2)-yLimVals(1))],'FontSize',9,'String',ptext);
+            else
+                coefficients(1,1:6)=[coefficients1(2) coefficients2(2) coefficients3(2) coefficients4(2) coefficients5(2) coefficients6(2)];
+                coefficients(2,1:6)=[p1(2) p2(2) p3(2) p4(2) p5(2) p6(2)]
+                subplot(2,3,1);
+                ptext=sprintf('r= %f  p= %f',coefficients(1,1),coefficients(2,1));
+                yLimVals=get(gca,'YLim');
+                xLimVals=get(gca,'XLim');
+                text('Position',[xLimVals(1) yLimVals(1)-0.1*(yLimVals(2)-yLimVals(1))],'FontSize',9,'String',ptext);
+                subplot(2,3,4);
+                ptext=sprintf('r= %f  p= %f',coefficients(1,2),coefficients(2,2));
+                yLimVals=get(gca,'YLim');
+                xLimVals=get(gca,'XLim');
+                text('Position',[xLimVals(1) yLimVals(1)-0.1*(yLimVals(2)-yLimVals(1))],'FontSize',9,'String',ptext);
+                subplot(2,3,2);
+                ptext=sprintf('r= %f  p= %f',coefficients(1,3),coefficients(2,3));
+                yLimVals=get(gca,'YLim');
+                xLimVals=get(gca,'XLim');
+                text('Position',[xLimVals(1) yLimVals(1)-0.1*(yLimVals(2)-yLimVals(1))],'FontSize',9,'String',ptext);
+                subplot(2,3,5);
+                ptext=sprintf('r= %f  p= %f',coefficients(1,4),coefficients(2,4));
+                yLimVals=get(gca,'YLim');
+                xLimVals=get(gca,'XLim');
+                text('Position',[xLimVals(1) yLimVals(1)-0.1*(yLimVals(2)-yLimVals(1))],'FontSize',9,'String',ptext);
+                subplot(2,3,6);
+                ptext=sprintf('r= %f  p= %f',coefficients(1,5),coefficients(2,5));
+                yLimVals=get(gca,'YLim');
+                xLimVals=get(gca,'XLim');
+                text('Position',[xLimVals(1) yLimVals(1)-0.1*(yLimVals(2)-yLimVals(1))],'FontSize',9,'String',ptext);
+                subplot(2,3,3);
+                ptext=sprintf('r= %f  p= %f',coefficients(1,6),coefficients(2,6));
+                yLimVals=get(gca,'YLim');
+                xLimVals=get(gca,'XLim');
+                text('Position',[xLimVals(1) yLimVals(1)-0.1*(yLimVals(2)-yLimVals(1))],'FontSize',9,'String',ptext);
+            end
+        else
+            coefficients(1,1:5)=[coefficients1(2) coefficients2(2) coefficients3(2) coefficients5(2) coefficients6(2)];
+            coefficients(2,1:5)=[p1(2) p2(2) p3(2) p5(2) p6(2)]
+            subplot(1,3,1);
+            ptext=sprintf('r= %f  p= %f',coefficients(1,1),coefficients(2,1));
+            yLimVals=get(gca,'YLim');
+            xLimVals=get(gca,'XLim');
+            text('Position',[xLimVals(1) yLimVals(1)-0.1*(yLimVals(2)-yLimVals(1))],'FontSize',9,'String',ptext);
+            subplot(1,3,2);
+            ptext=sprintf('r= %f  p= %f',coefficients(1,2),coefficients(2,2));
+            yLimVals=get(gca,'YLim');
+            xLimVals=get(gca,'XLim');
+            text('Position',[xLimVals(1) yLimVals(1)-0.1*(yLimVals(2)-yLimVals(1))],'FontSize',9,'String',ptext);
+            subplot(1,3,3);
+            ptext=sprintf('r= %f  p= %f',coefficients(1,3),coefficients(2,3));
+            yLimVals=get(gca,'YLim');
+            xLimVals=get(gca,'XLim');
+            text('Position',[xLimVals(1) yLimVals(1)-0.1*(yLimVals(2)-yLimVals(1))],'FontSize',9,'String',ptext);
+            subplot(1,3,4);
+            ptext=sprintf('r= %f  p= %f',coefficients(1,4),coefficients(2,4));
+            yLimVals=get(gca,'YLim');
+            xLimVals=get(gca,'XLim');
+            text('Position',[xLimVals(1) yLimVals(1)-0.1*(yLimVals(2)-yLimVals(1))],'FontSize',9,'String',ptext);
         end
-    end
-    coefFolder=fullfile('F:','PL',analysisTypeText,animal,subFolderName);
-    if ~exist(coefFolder,'dir')
-        mkdir(coefFolder)
-    end
-    coefPathname=fullfile(coefFolder,coefImagename);
-    printtext=sprintf('print -dpng %s.png',coefPathname);
-    eval(printtext);
-    if writeCoefs==1
-        %write correlation coefficients, session numbers, and slope values
-        %to file:
+        set(gcf,'PaperPositionMode','auto')
+        if isempty(chNum)
+            chText='mean_across_channels';
+        else
+            chText=num2str(chNum);
+        end
+        if calcPartial==0
+            subFolderName=[analysisTypeText,'_coef_images'];
+        elseif calcPartial==1
+            subFolderName=[analysisTypeText,'_coef_images_partialcorr'];
+        end
         if excludeSessHighSSE==0
-            coefMatname=[chText,appendText,startEndTime,'_',analysisTypeText,'_coefs_',area];
+            coefImagename=[chText,appendText,startEndTime,'_',analysisTypeText,'_coefs_',area];
         elseif excludeSessHighSSE==1
             if excludeOutliers==0
-                coefMatname=[chText,appendText,startEndTime,'_',analysisTypeText,'_coefs_',area,'_goodSSE'];
+                coefImagename=[chText,appendText,startEndTime,'_',analysisTypeText,'_coefs_',area,'goodSSE'];
             elseif excludeOutliers==1
-                coefMatname=[chText,appendText,startEndTime,'_',analysisTypeText,'_coefs_',area,'_goodSSE_no_outliers_sl',num2str(slSigmaMultiple),'_C50',num2str(c50SigmaMultiple)];
+                coefImagename=[chText,appendText,startEndTime,'_',analysisTypeText,'_coefs_',area,'goodSSE_no_outliers_sl',num2str(slSigmaMultiple),'_C50',num2str(c50SigmaMultiple)];
             end
         end
-        subFolderName=[analysisTypeText,'_coef_mat'];
-        coefMatFolder=fullfile('F:','PL',analysisTypeText,animal,subFolderName);
-        coefMatPathname=fullfile(coefMatFolder,coefMatname);
-        if ~exist(coefMatFolder,'dir')
-            mkdir(coefMatFolder)
+        coefFolder=fullfile('F:','PL',analysisTypeText,animal,subFolderName);
+        if ~exist(coefFolder,'dir')
+            mkdir(coefFolder)
         end
-        if plotDiffC50_30==1
-            saveText=['save ',coefMatPathname,'.mat coefficients sessionSorted1 slopeNeuro sessionSorted2 slopePsycho matchPsycho c50 diffc50 minRate maxRate'];
-        else
-            saveText=['save ',coefMatPathname,'.mat coefficients sessionSorted1 slopeNeuro sessionSorted2 slopePsycho matchPsycho c50 minRate maxRate'];
+        coefPathname=fullfile(coefFolder,coefImagename);
+        printtext=sprintf('print -dpng %s.png',coefPathname);
+        eval(printtext);
+        if writeCoefs==1
+            %write correlation coefficients, session numbers, and slope values
+            %to file:
+            if excludeSessHighSSE==0
+                coefMatname=[chText,appendText,startEndTime,'_',analysisTypeText,'_coefs_',area];
+            elseif excludeSessHighSSE==1
+                if excludeOutliers==0
+                    coefMatname=[chText,appendText,startEndTime,'_',analysisTypeText,'_coefs_',area,'_goodSSE'];
+                elseif excludeOutliers==1
+                    coefMatname=[chText,appendText,startEndTime,'_',analysisTypeText,'_coefs_',area,'_goodSSE_no_outliers_sl',num2str(slSigmaMultiple),'_C50',num2str(c50SigmaMultiple)];
+                end
+            end
+            if calcPartial==0
+                subFolderName=[analysisTypeText,'_coef_mat'];
+            elseif calcPartial==1
+                subFolderName=[analysisTypeText,'_coef_mat_partialcorr'];
+            end
+            coefMatFolder=fullfile('F:','PL',analysisTypeText,animal,subFolderName);
+            coefMatPathname=fullfile(coefMatFolder,coefMatname);
+            if ~exist(coefMatFolder,'dir')
+                mkdir(coefMatFolder)
+            end
+            if plotDiffC50_30==1
+                saveText=['save ',coefMatPathname,'.mat coefficients sessionSorted1 slopeNeuro sessionSorted2 slopePsycho matchPsycho c50 diffc50 minRate maxRate'];
+            else
+                saveText=['save ',coefMatPathname,'.mat coefficients sessionSorted1 slopeNeuro sessionSorted2 slopePsycho matchPsycho c50 minRate maxRate'];
+            end
+            eval(saveText)
         end
-        eval(saveText)
     end
 end
