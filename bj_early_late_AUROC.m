be597af6737012332d9611ffaf16920bf53039a5
Xing Chen <x.chen1@ncl.ac.uk>
2013-08-17 20:58:40 +0100

    PROBMAT w time for test contrasts, roving

--- bj_early_late_AUROC.m
+++ bj_early_late_AUROC.m
@@ -3,15 +3,18 @@ if nargin<3||isempty(animals)
     animals=[{'blanco'} {'jack'}];
     % animals={'blanco'};
 end
-roving=0;
+roving=1;
 if nargin<4||isempty(areas)
     areas=[{'v4_1'} {'v4_2'} {'v1_1'} {'v1_2'}];
     areas=[{'v4_1'} {'v1_1'} {'v1_2_1'} {'v1_2_2'} {'v1_2_3'}];
     if roving==0
         areas=[{'v4_1'} {'v1_1'}];
+    elseif roving==1
+        areas=[{'v1_2_2'}];
+        areas=[{'v1_2_1'} {'v1_2_2'}];
     end
 end
-calcStats=1;
+calcStats=0;
 if calcStats==1
     fig1=figure('Color',[1,1,1],'Units','Normalized','Position',[0.1, 0.1, 0.3, 0.4]); %
     set(fig1, 'PaperUnits', 'centimeters', 'PaperType', 'A4', 'PaperOrientation', 'landscape', 'PaperPosition', [0.63452 0.63452 6.65 3.305]);
@@ -118,8 +121,6 @@ if calcStats==1
 end
 if roving==0
     pcpseslFig=figure('Color',[1,1,1],'Units','Normalized','Position',[0.12, 0.08, 0.4, 0.5]);
-elseif roving==1
-    pcpseslFig=figure('Color',[1,1,1],'Units','Normalized','Position',[0.12, 0.08, 0.4, 0.5]);
 end
 markerTexts='+x';
 for animalInd=1:length(animals)
@@ -129,6 +130,13 @@ for animalInd=1:length(animals)
         [sampleContrasts testContrasts]=area_metadata(area);
         channels=main_channels(animal,area);
         sessionNums=main_raw_sessions_final(animal,area,[],0);
+        if roving==1
+            if animalInd==1
+                pcpseslFig(areaInd)=figure('Color',[1,1,1],'Units','Normalized','Position',[0.12, 0.08, 0.4, 0.5]);
+            else
+                pcpseslFig(areaInd)
+            end
+        end
         for sampleContrastsInd=1:length(sampleContrasts)
             sampleContrast=sampleContrasts(sampleContrastsInd);
             testContrast=testContrasts(sampleContrastsInd,:); 
@@ -137,7 +145,11 @@ for animalInd=1:length(animals)
             loadText=['load F:\PL\ROC_zero_one\',animal,'\new_vs_old_sglroc3acrosschannels\cumulative_ROCs_old_new_',area,'_',num2str(sampleContrast),'_cutoff10.mat'];
             eval(loadText)
             allMeanPerf=all_rocvals;
-            subplot(length(areas),2,animalInd+2*(areaInd-1));
+            if roving==0
+                subplot(length(areas),2,animalInd+2*(areaInd-1));
+            elseif roving==1
+                subplot(length(sampleContrasts),2,animalInd+2*(sampleContrastsInd-1));
+            end
             markerText=markerTexts(2);markerS=8;
             for i=1:length(testContrast)
                 plot(1:size(allMeanPerf,1),allMeanPerf(:,1+i)*100,'Color',colmapText(i,:),'LineStyle','none','Marker',markerText,'MarkerFaceColor',colmapText(i,:),'MarkerEdgeColor',colmapText(i,:),'MarkerSize',markerS);hold on%'MarkerFaceColor',[1/i 1/i 1/i],
@@ -145,10 +157,10 @@ for animalInd=1:length(animals)
                     plotLinear=0;
                     startpoint5=1;
                 else
-                    plotLinear=1;
-                    startpoint5=0;
+                    plotLinear=0;
+                    startpoint5=1;
                 end
-                [chiselinearTemp(i,:) chiseTemp(i,:) coefperflinearTemp(i,:) coefperfTemp(i,:) aRSlinearTemp(i,:) aRSTemp(i,:)]=bj_linearexpo_fitting(testContrast,allMeanPerf(:,1+i)*100,i,startpoint5,'ROC',plotLinear);
+                [chiselinearTemp(i,:) chiseTemp(i,:) coefperflinearTemp(i,:) coefperfTemp(i,:) aRSlinearTemp(i,:) aRSTemp(i,:)]=bj_linearexpo_fitting(testContrast,allMeanPerf(:,1+i)*100,i,startpoint5,'ROC',plotLinear,[],[]);
             end
             xlim([0 size(allMeanPerf,1)+1]);
             ylim([0 100]);
@@ -174,7 +186,7 @@ for animalInd=1:length(animals)
                 title(['Monkey ',num2str(animalInd)]);
                 if animalInd==1
                     xlabel('session number');
-                    ylabel('AUROC');
+                    ylabel('PROBMAT');
                 end
             end
         end
