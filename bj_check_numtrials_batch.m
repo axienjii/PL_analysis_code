845dba4963f5fe25ebb84c9d0f0eff14615042cd
Xing Chen <x.chen1@ncl.ac.uk>
2014-03-17 13:54:42 +0000

    corrupt/missing data B ch 4 sess 322

--- bj_check_numtrials_batch.m
+++ bj_check_numtrials_batch.m
@@ -10,16 +10,16 @@ function bj_check_numtrials_batch(animal,area)
 %431 onwards: 36 conditions (roving with flankers)
 %Remember to adjust folder to which ROC text files are written in function
 %blanco_SE_V1_2_roc!!!
-notChopped=1;
+notChopped=0;
 chopSessions=311;%311 & 333
 comparisonType=1;
 % animal='blanco';
 % animal='jack';
 % area='v1_2';
-animals=[{'blanco'} {'jack'}];
-animals=[{'jack'}];
-areas=[{'v4_1'} {'v4_2'} {'v1_1'} {'v1_2'}];
-areas=[{'v4_2'}];
+% animals=[{'blanco'} {'jack'}];
+% animals=[{'jack'}];
+% areas=[{'v4_1'} {'v4_2'} {'v1_1'} {'v1_2'}];
+areas=[{'v4_1'}];
 animals=[{'blanco'}];
 for animalInd=1:length(animals)
     animal=animals{animalInd};
@@ -42,13 +42,15 @@ for animalInd=1:length(animals)
                     chStr=[num2str(channel),'_',num2str(sessionNums(i)),'_',num2str(sampleContrast),'.mat'];
                     matPath=fullfile(matFolder,chStr);
                     valsText=['load ',matPath,' matarray'];
-                    eval(valsText);
-                    for cond=1:length(testContrast)
-                        for epoch=1:5
-                            sizeAll(cond,epoch)=length(matarray{cond,epoch});
+                    if exist(matPath,'file')
+                        eval(valsText);
+                        for cond=1:length(testContrast)
+                            for epoch=1:5
+                                sizeAll(cond,epoch)=length(matarray{cond,epoch});
+                            end
                         end
+                        sizeAllChs{h}=sizeAll;%compile across channels
                     end
-                    sizeAllChs{h}=sizeAll;%compile across channels
                 end
                 template=sizeAllChs{1};%arbitrarily use first channel as baseline for comparison
                 
@@ -61,10 +63,12 @@ for animalInd=1:length(animals)
                             else
                                 numCheck=sizeAllChs{h}(cond,epoch);
                                 if numCheck~=template(cond,epoch)
+%                                     if channels(h)~=4&&sessionNums(i)~=322
                                     channels(h)
                                     cond
                                     epoch
                                     sessionNums(i)
+%                                     end
                                 end
                             end
                         end
